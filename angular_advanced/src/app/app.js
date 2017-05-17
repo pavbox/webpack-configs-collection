b065f937a1b7e4a3b429658565afde5574896fbd
pavbox <pavboxdev@gmail.com>
2017-05-17 12:00:01 +0300

    initial default angular advanced

---
+++ angular_advanced/src/app/app.js
@@ -0,0 +1,6 @@
+
+import '../styles/best-styles.css';
+
+import { multiply } from './math';
+console.log(multiply(25));; // 625
+// console.log(num);
---
+++ angular_advanced/src/app/math.js
@@ -0,0 +1,5 @@
+
+
+export function multiply(num) {
+  return (num * num);
+}
